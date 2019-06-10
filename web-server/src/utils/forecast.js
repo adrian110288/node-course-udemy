b8b82a7ed7c4a579fd7b335f179f8a3e4486a930
Adrian Lesniak <adrian110288@gmail.com>
2019-06-10 19:48:27 +0100

    Finish web-server project.

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -3,14 +3,19 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/3a246c5b28a535bb1019e7f541e5b05b/' + latitude + ',' + longitude
 
-    request({ url, json: true }, (error, { body }) => {
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    }) => {
 
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
