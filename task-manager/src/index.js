947226c686ddfaa0f9bfe0b7641cf792d0a5d80b
Adrian Lesniak <adrian110288@gmail.com>
2019-06-12 21:15:24 +0100

    Delete users and tasks.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -75,6 +75,23 @@ app.patch('/users/:id', async (req, res) => {
 
 })
 
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            res.status(404).send()
+        }
+
+        res.send(user)
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -111,6 +128,23 @@ app.patch('/tasks/:id', async (req, res) => {
 
     } catch (error) {
         res.status(400).send(error)
+    }
+
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (error) {
+        res.status(500).send(error)
     }
 
 })
