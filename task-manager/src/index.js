3a1a6473fb8f77e34570ab7c28d496b3fb3a060b
Adrian Lesniak <adrian110288@gmail.com>
2019-06-22 14:18:40 +0100

    Filter tasks.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
+const logger = require('morgan')
 const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -7,6 +8,11 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+logger.token('body', function (req, res) {
+    return JSON.stringify(req.body)
+});
+
+app.use(logger(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 app.use(express.json())
 
 app.use('/users', userRouter)
