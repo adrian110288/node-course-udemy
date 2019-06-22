f8daf9b63dada7a88ae4ba2a782bbbde37120d28
Adrian Lesniak <adrian110288@gmail.com>
2019-06-22 14:30:40 +0100

    Sorting tasks.

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -73,9 +73,15 @@ router.delete('/:id', auth, async (req, res) => {
 router.get('/', auth, async (req, res) => {
 
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
@@ -85,7 +91,8 @@ router.get('/', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         }).execPopulate()
 
